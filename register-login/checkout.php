c11ca53439ec541610534b35cc0b717f0dbf98fd
Daniel Kulakouski <dan.kulakouski@gmail.com>
2021-06-03 22:24:52 -0400

    updated styling of checkout page

--- register-login/checkout.php
+++ register-login/checkout.php
@@ -12,27 +12,56 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 ?>
+<!-- Bootstrap CSS CDN -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
 
-<h1>Checkout</h1>
-<form method="post">
-    <input type="submit" name="checkout" value="Checkout">
-</form>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <!-- Bootstrap JS -->
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 
-<?php
-if(isset($_POST["checkout"]) && isset($_GET["bid"])) {
-    $id = $_GET["bid"];
-    // echo $id;
-    $queryTest = "SELECT count FROM book WHERE book_id = $id";
-    $results = $conn -> query($queryTest);
-    $row = mysqli_fetch_assoc($results);
-    $count = $row['count'];
+    <!-- Our Custom CSS -->
+    <link rel="stylesheet" href="../book.css">
+</head>
+<body>
+    <?php require "../header.php";
 
-    if($count > 0) {
-        $count--;
-        $checkout = "UPDATE book SET count = $count WHERE book_id = $id";
-        $results = $conn -> query($checkout);
+    if(isset($_GET["bid"])) {
+        $id = $_GET["bid"];
+        // echo $id;
+        $query = "SELECT * FROM book WHERE book_id = $id";
+        $results = $conn -> query($query);
+        $book = mysqli_fetch_assoc($results);
+        $key = $book["original_key"];
+        $count = $book['count'];
+        echo '<title>Checkout</title>';
+        echo '<div class="d-flex justify-content-center">';
+            echo "<img src='http://covers.openlibrary.org/b/olid/$key.jpg' class='p-2'>";
+            echo "Copies Left: $count";
+        echo '</div>';
+        
+        echo '<div class="d-flex justify-content-center">';
+            echo '<form method="post">';
+                echo '<input type="submit" name="checkout" value="Checkout" class="btn btn-secondary p-2 m-4">';
+            echo '</form>';
+        echo '</div>';
+        
+        if(isset($_POST["checkout"])) {
+            if($count > 0) {
+                $count--;
+                $checkout = "UPDATE book SET count = $count WHERE book_id = $id";
+                $results = $conn -> query($checkout);
+            } else {
+                echo "Out of stock!";
+            }
+        }
     } else {
-        echo "Out of stock!";
+        echo "Missing book ID!";
     }
-}
-?>
+    ?>
+</body>
+</html>
