45b2870e561f0787c949fbf6f0cb07a23397e804
Alex Ao Shen <ao.shen@gmail.com>
2021-07-05 22:24:58 -0400

    created trade up

--- inventory.php
+++ inventory.php
@@ -9,6 +9,83 @@ if(!isset($_SESSION["uid"])) {
 
 $uid = $_SESSION["uid"];
 
+$remove_sql = "";
+$trade_sql = "";
+
+function processTradeUp() {
+    global $conn, $uid;
+
+    $trade_up_books = explode(',', $_POST["trade_up"]);
+
+    if(count($trade_up_books) != 5) return false;
+    
+    $trade_up_books_query = "";
+    for($i = 0; $i < 5; $i++) {
+        $trade_up_books_query = $trade_up_books_query."'$trade_up_books[$i]',";
+    }
+    $trade_up_books_query = rtrim($trade_up_books_query, ", ");
+
+    $sql = "SELECT rarity
+            FROM book
+            WHERE book_id IN ($trade_up_books_query)
+            GROUP BY rarity";
+
+    $result = $conn->query($sql);
+
+    $row_cnt = $result->num_rows;
+
+    if($row_cnt != 1) return false;
+
+    $row = $result->fetch_assoc();
+
+    $rarity = $row["rarity"] + 1;
+
+    $trade_sql = "SELECT book_id
+            FROM (SELECT collection_id 
+                    FROM collection_book 
+                    WHERE book_id IN ($trade_up_books_query)
+                    GROUP BY collection_id) c
+            NATURAL JOIN collection_book
+            NATURAL JOIN book
+            WHERE rarity = '$rarity'
+            GROUP BY book_id";
+    $result = $conn->query($trade_sql);
+
+    $possible_books = [];
+
+    while($row = $result->fetch_assoc()) {
+        array_push($possible_books, $row["book_id"]);
+    }
+
+    if(count($possible_books) == 0) return false;
+
+    $remove_sql = "";
+    for($i = 0; $i < 5; $i++) {
+        $remove_sql = "DELETE FROM user_inventory WHERE user_id = '$uid' AND book_id = '$trade_up_books[$i]';";
+        $result = $conn->query($remove_sql);
+    }
+
+    $book_idx = rand(0, count($possible_books) - 1);
+
+    // add to inventory
+
+    $inventory_query = '("'.$uid.'","'.$possible_books[$book_idx].'","1")';
+
+    $sql = sprintf('
+    INSERT INTO user_inventory (user_id,book_id,amount) 
+    VALUES %s
+    ON DUPLICATE KEY UPDATE amount=amount+1;',
+    $inventory_query);
+
+    $result = $conn->query($sql);
+    
+    return true;
+}
+
+if(isset($_POST["trade_up"])) {
+    processTradeUp();
+}
+
 $inventory_uid_query = 'WHERE i.user_id = "'.$uid.'"';
 
 $sql = sprintf('
@@ -37,6 +114,9 @@ usort($inventory_books, function ($a, $b) {
 ?>
 
 <script>
+
+console.log(`<?php echo ($remove_sql); ?>`);
+console.log(`<?php echo ($trade_sql); ?>`);
 
 let sql = `<?php echo ($sql); ?>`;
 let inventory = [<?php 
@@ -85,16 +165,43 @@ console.log(inventory);
         My Inventory
     </h4>
 
-	<div class="wrapper">
+	<div class="wrapper container">
 
         <!-- Page Content Holder -->
         <div id="content">
+            <div class="d-flex justify-content-between align-items-center inventory-menu mb-2">
+                <div class="inventory-menu-item form-group row">
+                    <label for="sort-select" class="col-sm-5 col-form-label">Sort by:</label>
+                    <select id="sort-select" class="col-sm-7 custom-select" onchange="onSortChanged()">
+                        <option selected value="rarity">Rarity</option>
+                        <option value="amount">Amount</option>
+                    </select>
+                </div>
+                <div id="default-menu" class="inventory-menu-item form-group row">
+                    <button id="btn-trade-up" type="button" class="btn btn-success" onclick="tradeUpClicked()">Trade Up</button>
+                    <button id="btn-edit-deck" type="button" class="btn btn-primary" onclick="editDeckClicked()">Edit Deck</button>
+                </div>
+                <div id="trade-up-menu" class="inventory-menu-item form-group row" style="display: none;">
+                    <button id="btn-trade-up-fill-3" type="button" class="btn btn-primary" onclick="tradeUpFill3Clicked()">Autofill 3★</button>
+                    <button id="btn-trade-up-fill-4" type="button" class="btn btn-primary" onclick="tradeUpFill4Clicked()">Autofill 4★</button>
+                    <button id="btn-trade-up-init" type="button" class="btn btn-success" onclick="tradeUpInitClicked()">Initiate Trade Up</button>
+                    <button id="btn-trade-up-cancel" type="button" class="btn btn-danger" onclick="tradeUpCancelClicked()">Cancel Trade Up</button>
+                </div>
+            </div>
+
+            <div class="collapse" id="trade-up-collapse">
+                <div class="card card-body">
+                    <div id="trade_up_container" class="inventory_container trade_up_container">
+                    </div>
+                </div>
+            </div>
+
             <div class="inventory_container">
                 <?php
                 foreach($inventory_books as $j => $book) {
                     $rarity = $book["rarity"];
                     ?>
-                    <div class="book_box book_box_rarity_<?php echo $rarity; ?>">
+                    <div id="book-box-<?php echo $j; ?>" class="book_box book_box_rarity_<?php echo $rarity; ?>" style="order: <?php echo $j; ?>;">
                         <div id="book-container3d-<?php echo $j; ?>" class="book-container3d">
                             <div class="book3d">
                                 <div class="book3d-cover cover-title" id="cover-title-<?php echo $j; ?>"><?php echo $book["title"]; ?></div>
@@ -121,15 +228,11 @@ console.log(inventory);
                                 data-trigger="manual">
                             </div>
                         </div>
-                        <?php if($book["amount"] != 1) { ?>
-                        <div class="amount-container">
+                        <div class="amount-container" <?php if($book["amount"] == 1) echo 'style="display:none;"'; ?>>
                             <div class="text-center amount">
-                                <?php
-                                    echo '×'.$book["amount"];
-                                ?>
+                                ×<span id="book-amount-<?php echo $j; ?>"><?php echo $book["amount"]; ?></span>
                             </div>
                         </div>
-                        <?php } ?>
                     </div>
                     <?php
                 }
--- js/inventory.js
+++ js/inventory.js
@@ -3,54 +3,182 @@ empty_cover_img.src = "assets/empty_cover_small.png";
 
 let popoverIdx = -1;
 
+let menu = "default";
+let tradeUpInventory = {};
+
+let tradeUpIdx = 0;
+
 function onBookClicked(i) {
     console.log(i + " clicked!");
 
-    if (popoverIdx != -1) {
-        console.log(popoverIdx + " hid!");
-        $(`#book-container3d-${popoverIdx}`).removeClass("book-focused");
-        $(`#book-popover-${popoverIdx}`).popover('hide');
-    }
+    if (menu == "default") {
+        if (popoverIdx != -1) {
+            console.log(popoverIdx + " hid!");
+            $(`#book-container3d-${popoverIdx}`).removeClass("book-focused");
+            $(`#book-popover-${popoverIdx}`).popover('hide');
+        }
 
-    if (popoverIdx == i) {
-        popoverIdx = -1;
-    } else {
-        popoverIdx = i;
-        console.log(popoverIdx + " shown!");
-        $(`#book-container3d-${popoverIdx}`).addClass("book-focused");
+        if (popoverIdx == i) {
+            popoverIdx = -1;
+        } else {
+            popoverIdx = i;
+            console.log(popoverIdx + " shown!");
+            $(`#book-container3d-${popoverIdx}`).addClass("book-focused");
 
-        $(`#book-popover-${popoverIdx}`).popover({
-            html: true,
-            fallbackPlacement: ['bottom'],
-            boundary: "scrollParent",
-            content: function() {
-                return '<div id="book-popover-content-current">Loading...</div>';
-            }
-        });
-        $(`#book-popover-${popoverIdx}`).popover('show');
-
-        let xhr = new XMLHttpRequest();
-        xhr.open("GET", `book_detail_popover.php?id=${inventory[popoverIdx].book_id}`, true);
-        xhr.onload = function(e) {
-            if (xhr.readyState === 4) {
-                if (xhr.status === 200) {
-                    let response = xhr.responseText;
-                    console.log(response);
-                    $("#book-popover-content-current").html(response);
-                    $(`#book-popover-${popoverIdx}`).popover('update');
-                    $(function() {
-                        $(".col-md-2").tooltip('enable');
-                    });
-                } else {
-                    console.error(xhr.statusText);
+            $(`#book-popover-${popoverIdx}`).popover({
+                html: true,
+                fallbackPlacement: ['bottom'],
+                boundary: "scrollParent",
+                content: function() {
+                    return '<div id="book-popover-content-current">Loading...</div>';
                 }
+            });
+            $(`#book-popover-${popoverIdx}`).popover('show');
+
+            let xhr = new XMLHttpRequest();
+            xhr.open("GET", `book_detail_popover.php?id=${inventory[popoverIdx].book_id}`, true);
+            xhr.onload = function(e) {
+                if (xhr.readyState === 4) {
+                    if (xhr.status === 200) {
+                        let response = xhr.responseText;
+                        console.log(response);
+                        $("#book-popover-content-current").html(response);
+                        $(`#book-popover-${popoverIdx}`).popover('update');
+                        $(function() {
+                            $(".col-md-2").tooltip('enable');
+                        });
+                    } else {
+                        console.error(xhr.statusText);
+                    }
+                }
+            };
+            xhr.onerror = function(e) {
+                console.error(xhr.statusText);
+            };
+            xhr.send(null);
+        }
+    } else if (menu == "trade-up") {
+        if (inventory[i].amount > 0 && Object.keys(tradeUpInventory).length < 5) {
+            inventory[i].amount--;
+            if (inventory[i].amount == 1) {
+                $(`#book-amount-${i}`).parent().parent().css("display", "none");
+            } else if (inventory[i].amount == 0) {
+                $(`#book-amount-${i}`).parent().parent().parent().css("display", "none");
+            } else {
+                $(`#book-amount-${i}`).html(inventory[i].amount);
             }
-        };
-        xhr.onerror = function(e) {
-            console.error(xhr.statusText);
-        };
-        xhr.send(null);
+            tradeUpIdx++;
+            tradeUpInventory[tradeUpIdx] = i;
+            $(`#trade_up_container`).append(`
+            <div id="book-box-trade-up-${tradeUpIdx}" class="book_box book_box_rarity_${inventory[i].rarity} book_box_rarity_trade_up">
+            ${$(`#book-box-${i}`).html()
+            .replace("book-amount", "book-amount-trade-up")}
+            </div>`);
+
+            $(`#book-box-trade-up-${tradeUpIdx} .amount-container`).remove();
+
+            let j = tradeUpIdx;
+            $(`#book-box-trade-up-${tradeUpIdx}`).click(function(e) {
+                onTradeUpBookClicked(j);
+            });
+        }
     }
+}
+
+function onTradeUpBookClicked(i) {
+    console.log("Trade Up " + i + " clicked!");
+
+    $(`#book-box-trade-up-${i}`).remove();
+
+    inventory[tradeUpInventory[i]].amount++;
+    
+    j = tradeUpInventory[i];
+    if (inventory[j].amount == 1) {
+        $(`#book-amount-${j}`).parent().parent().parent().show();
+    } else if (inventory[j].amount == 2) {
+        $(`#book-amount-${j}`).parent().parent().show();
+        $(`#book-amount-${j}`).html(inventory[j].amount);
+    } else {
+        $(`#book-amount-${j}`).html(inventory[j].amount);
+    }
+
+    delete tradeUpInventory[i]; 
+}
+
+function onSortChanged() {
+    let selectedVal = $("#sort-select option:selected").val()
+    console.log(selectedVal);
+
+    let maxOrder = 0;
+
+    for (let j = 0; j < inventory.length; j++) {
+        if (selectedVal == "amount") {
+            maxOrder = Math.max(maxOrder, inventory[j].amount);
+        } else if (selectedVal == "rarity") {
+            order = Math.max(maxOrder, j);
+        }
+    }
+
+    for (let j = 0; j < inventory.length; j++) {
+        let order = -1;
+        if (selectedVal == "amount") {
+            order = maxOrder - inventory[j].amount;
+        } else if (selectedVal == "rarity") {
+            order = j;
+        }
+        $(`#book-box-${j}`).css("order", order);
+    }
+}
+
+function tradeUpClicked() {
+    menu = "trade-up";
+    $('#trade-up-collapse').collapse('show');
+    $('#default-menu').slideUp(complete = () => {
+        $('#trade-up-menu').slideDown();
+    });
+}
+
+function editDeckClicked() {
+
+}
+
+function tradeUpFill3Clicked() {
+    for (let j = 0; j < inventory.length && Object.keys(tradeUpInventory).length < 5; j++) {
+        if(inventory[j].rarity == 3) {
+            while(inventory[j].amount > 0 && Object.keys(tradeUpInventory).length < 5) {
+                onBookClicked(j);
+            }
+        }
+    }
+}
+
+function tradeUpFill4Clicked() {
+    for (let j = 0; j < inventory.length && Object.keys(tradeUpInventory).length < 5; j++) {
+        if(inventory[j].rarity == 4) {
+            while(inventory[j].amount > 0 && Object.keys(tradeUpInventory).length < 5) {
+                onBookClicked(j);
+            }
+        }
+    }
+}
+
+function tradeUpInitClicked() {
+
+    let requestBody = {trade_up: []};
+    
+    for (const book in tradeUpInventory) {
+        requestBody.trade_up.push(inventory[tradeUpInventory[book]].book_id);
+    }
+
+    redirectPost("#", requestBody);
+}
+
+function tradeUpCancelClicked() {
+    menu = "default";
+    $('#trade-up-collapse').collapse('hide');
+    $('#trade-up-menu').slideUp(complete = () => {
+        $('#default-menu').slideDown();
+    });
 }
 
 $(document).ready(function() {
